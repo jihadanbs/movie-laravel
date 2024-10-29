16936b034700b0bd8e8f5ea7a40f2bb103051b5a
jihadanbs <jihadanbs11@gmail.com>
2024-10-29 23:09:29 +0700

    example middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Middleware\CheckMembership;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('/movie', function () use ($movie) {
+//     return $movie;
+// });
+
+Route::get('/movie', function () {
+    return 'movie';
+})->middleware(CheckMembership::class);
+
+Route::get('/pricing', function () {
+    return 'please, buy a membership';
+    // return view('pricing');
 });
