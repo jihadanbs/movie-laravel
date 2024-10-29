794a5e6d45a7415c763401d69f20d97272088db3
jihadanbs <jihadanbs11@gmail.com>
2024-10-30 04:28:56 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-
+        // catatan
         // untuk pemanggilan middleware yang ada di class CheckMembership
         // agar tidak perlu memanggil middleware manual di controller
         // lebih clean
